baddcbaa0aa88c3fdbacf81b3455456965bd2de5
joao paulo <joaopaulo_marquesj@outlook.com>
2022-05-01 18:44:12 -0300

    Versao inicial do front LPIII

---
+++ app/src/main/java/com/example/beta/telaPerguntas.kt
@@ -0,0 +1,11 @@
+package com.example.beta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class telaPerguntas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_perguntas)
+    }
+}
